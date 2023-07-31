59e38f3671ea6fdd71bdf691df4d0d80253b0960
SergiyZatsarinniy <Zatsarinniys@gmail.com>
2023-08-01 01:40:52 +0300

    summary

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,42 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import React, { Component } from 'react';
+
+class App extends Component {
+  state = {
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  };
+
+  handleIncrement = state => {
+    this.setState(prevState => ({
+      [state]: prevState[state] + 1,
+    }));
+  };
+
+  render() {
+    return (
+      <div>
+        <section>
+          <p>Please leave feedback</p>;
+          <button type="button" onClick={() => this.handleIncrement('good')}>
+            Good
+          </button>
+          <button type="button" onClick={() => this.handleIncrement('neutral')}>
+            Neutral
+          </button>
+          <button type="button" onClick={() => this.handleIncrement('bad')}>
+            Bad
+          </button>
+        </section>
+        <section>
+          <p>Statistics</p>
+          <p>Good:{this.state.good}</p>
+          <p>Neutral:{this.state.neutral}</p>
+          <p>Bad:{this.state.bad}</p>
+        </section>
+      </div>
+    );
+  }
+}
+
+export default App;
